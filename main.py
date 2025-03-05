a4f2895cb059b3cd3aa5536ba36aec6cfa2bd4b0
Robert Walker <bellrat@gmail.com>
2025-03-05 00:13:24 +0000

    added args

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
+import sys
+
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
-    print("--- Begin report of books/frankenstein.txt ---")
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <book_path>")
+        sys.exit(1)
+    # book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
+    print(f"--- Begin report of {book_path} ---")
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
-    print()
-    char_dict = get_char_counts(text)
-    char_list = sorted(char_dict, key=char_dict.get, reverse=True)
-    for cd in char_list:
-        print(f"The '{cd}' character was found {char_dict[cd]} times")
-    print("--- End report ---")
-
-def get_char_counts(text):
-    lower_text = text.lower()
-    char_dict = {}
-    for c in lower_text:
-        if not c.isalpha():
-            next       
-        elif c in char_dict:
-            char_dict[c] += 1
-        else:
-            char_dict[c] = 1
-    return char_dict
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 main()
